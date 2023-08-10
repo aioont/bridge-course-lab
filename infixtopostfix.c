368bd2a45738c03077ccc9ede0df92eeb2365999
aioont <aioont8@gmail.com>
2023-08-10 12:04:10 +0530

    Update infixtopostfix.c

--- infixtopostfix.c
+++ infixtopostfix.c
@@ -1,49 +1,61 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <ctype.h>
 
-#define MAX 100
+#define SIZE 50
 
-char stack[MAX];
-int top=-1;
-void push(char val) {
-    stack[++top]=val;
-   }
-void pop() {
-  if(top==-1)
-     return '\0';
-  else
-     return(stack[top--]);
+char stack[SIZE];
+int top = -1; /* Global declarations */
+
+void push(char elem) { /* Function for PUSH operation */
+    stack[++top] = elem;
 }
-void main() {
-  int i=0,k=0;
-  char infix[20], postfix[20],val;
-  printf("Enter the infix expression : ");
-  scanf("%s", infix);
-  push('#');
-  printf("Your INFIX expression : %s", infix);
-  len=strlen(infix);
-  for(i=0;i<len;i++)
-     {
-      if(ch=='(')
-         push(ch);
-     else 
-         if(isalnum(ch))
-            postfix[k++]=ch;
-         else
-            if(ch==')')
-              {
-               while(stack[top]!='(')
-                  postfix[k++]=pop();
-               val=pop();
-              }
-            else {
-               while(pre(stack[top])>=pre(ch))
-                     postfix[k++]=pop();
- 	    push(ch);
-            }
-     }
-    while(stack[top]!='#')
- 	postfix[k++]=pop();
-    postfix[k]='\0';
-     
+
+char pop() { /* Function for POP operation */
+    return stack[top--];
+}
+
+int pr(char symbol) { /* Function for precedence */
+    if (symbol == '^') /* exponent operator, highest precedence */
+        return 3;
+    else if (symbol == '*' || symbol == '/')
+        return 2;
+    else if (symbol == '+' || symbol == '-') /* lowest precedence */
+        return 1;
+    else
+        return 0;
+}
+
+int main() { /* Main Program */
+    char infix[50], postfix[50], ch, elem;
+    int i = 0, k = 0;
+    
+    printf("Enter Infix Expression: ");
+    scanf("%s", infix);
+    
+    push('#');
+    
+    while ((ch = infix[i++]) != '\0') {
+        if (ch == '(')
+            push(ch);
+        else if (isalnum(ch))
+            postfix[k++] = ch;
+        else if (ch == ')') {
+            while (stack[top] != '(')
+                postfix[k++] = pop();
+            elem = pop(); /* Remove ( */
+        } else { /* Operator */
+            while (pr(stack[top]) >= pr(ch))
+                postfix[k++] = pop();
+            push(ch);
+        }
+    }
+    
+    while (stack[top] != '#') /* Pop from stack till empty */
+        postfix[k++] = pop();
+    
+    postfix[k] = '\0'; /* Make postfix a valid string */
+    
+    printf("\nPostfix Expression = %s\n", postfix);
+    
+    return 0;
+}
