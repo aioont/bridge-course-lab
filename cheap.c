b60fb26da5176a2d5448a575de29d5bcec451d8b
aioont <aioont8@gmail.com>
2023-10-05 02:29:32 +0530

    Update cheap.c

--- cheap.c
+++ cheap.c
@@ -1,116 +1,103 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<stdlib.h>
+int arr[100],n=0;
 
-void swap(int heap[], int i, int j) {
-   int temp = heap[i];
-   heap[i] = heap[j];
-   heap[j] = temp;
+void display()
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("%d  ",arr[i]);
+    }
 }
 
-int insertElement(int heap[], int n, int element) {
-   if (n >= 100) {
-      printf("Heap is full, can't insert\n");
-      return n;
-   }
-   n++;
-   int i = n - 1;
-   heap[i] = element;
-  
-   while (i > 0 && heap[(i - 1) / 2] < heap[i]) {
-      int j = (i - 1) / 2;
-      swap(heap, i, (i - 1) / 2);
-      i = (i - 1) / 2;
-   }
-   return n;
+void maxheapify(int i)
+{
+    int largest=i;
+    int left=2*i+1;
+    int right=2*i+2;
+    if(left<n && arr[left]>arr[largest])
+    {
+        largest=left;
+    }
+    if(right<n && arr[right]>arr[largest])
+    {
+        largest=right;
+    }
+    if(largest!=i)
+    {
+        int temp=arr[i];
+        arr[i]=arr[largest];
+        arr[largest]=temp;
+        maxheapify(largest);
+    }
+    
 }
 
-void maxHeapify(int heap[], int n, int i) {
-   int largest = i;
-   int left = 2 * i + 1;
-   int right = 2 * i + 2;
-
-   if (left < n && heap[left] > heap[largest])
-      largest = left;
-  
-   if (right < n && heap[right] > heap[largest])
-      largest = right;
-  
-   if (largest != i) {
-      swap(heap, i, largest);
-      maxHeapify(heap, n, largest);
-   }
+void buildheap()
+{
+    for(int i=(n/2)-1;i>=0;i--)
+    {
+        maxheapify(i);
+    }
 }
 
-void buildMaxHeap(int heap[], int n) {
-   for (int i = n / 2 - 1; i >= 0; i--)
-      maxHeapify(heap, n, i);
+void insert(int value)
+{
+    arr[n++]=value;
+    buildheap();
+
 }
 
-void displayHeap(int heap[], int n) {
-   printf("MAX heap:\n");
-   for (int i = 0; i < n; i++)
-      printf("%d ", heap[i]);
-   printf("\n");
+void delete()
+{
+    if (n > 0)
+    {
+        arr[0] = arr[n - 1];
+        n--;
+        maxheapify(0);
+    }
+    else
+    {
+        printf("Heap is empty. Cannot delete.\n");
+    }
 }
 
-int deleteMaxElement(int heap[], int n) {
-   if(n<=0) {
-       printf("Heap is empty.\n");
-       return n;
-   }
-  
-  int root=heap[0];
-  heap[0]=heap[n-1];
-  n--;
-  maxHeapify(heap, n, 0);
-
-  return n; 
-}
-
-void sort(int heap[], int n) {
-   int val = 0;
-   int size = n;  // Store the initial size of the heap
-   while (n > 0) {
-      val = deleteMaxElement(heap, n);
-      heap[n] = val;
-      n--;
-   }
-   displayHeap(heap, size);  // Display only the initially stored elements
+void sort()
+{
+    printf("\nMAX heap : ");
+    while(n>0)
+    {
+        printf("%d - ",arr[0]);
+        arr[0]=arr[n-1];
+        n--;
+        buildheap();
+    }
 }
 
 
-int main() {
-   int heap[100];
-   int n = 0;
-   int choice, element;
-   while (1) {
-      printf("\n1. Insert element\n");
-      printf("2. Display\n");
-      printf("3. Delete\n");
-      printf("4. Exit\n");
-      printf("5. Sort \n");
-      printf("Enter choice: ");
-      scanf("%d", &choice);
-      
-      switch (choice) {
-         case 1:
-            printf("Enter element to insert: ");
-            scanf("%d", &element);
-            n = insertElement(heap, n, element);
-            break;
-         case 2:
-            displayHeap(heap, n);
-            break;
-         case 3:
-            n=deleteMaxElement(heap,n);
-            break;
-         case 4:
-            return 0;
-         case 5:
-            sort(heap, n);
-            break;
-         default:
-            printf("Invalid choice\n");
-      }
-   }
-   return 0;
+void main()
+{
+    int choice,value;
+    while(1)
+    {
+        printf("\n1.Instert\n2.Delete\n3.Display\n4.Sort\n5.Exit\n\n");
+        printf("enter your choice");
+        scanf("%d",&choice);
+        switch(choice)
+        {
+            case 1:printf("enter the value to insert");
+                    scanf("%d",&value);
+                    insert(value);
+                    break;
+            case 2:delete();
+                 break;
+            case 3:display();
+                break;
+            case 4:
+                sort();
+                display();
+                break;
+            case 5:exit(0);
+        }
+    }
 }
