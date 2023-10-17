054c761317f4acd743388b17f0bb2e8cd32d5487
aioont <aioont8@gmail.com>
2023-10-17 12:57:46 +0530

    Add files via upload

---
+++ ll.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to insert a node at the end of the linked list
+void insertAtEnd(struct Node** head, int data) {
+    struct Node* newNode = createNode(data);
+    if (*head == NULL) {
+        *head = newNode;
+        return;
+    }
+    struct Node* current = *head;
+    while (current->next != NULL) {
+        current = current->next;
+    }
+    current->next = newNode;
+}
+
+// Function to display the linked list
+void displayList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        printf("%d -> ", current->data);
+        current = current->next;
+    }
+    printf("NULL\n");
+}
+
+// Function to free the memory allocated for the linked list
+void freeList(struct Node* head) {
+    struct Node* current = head;
+    while (current != NULL) {
+        struct Node* temp = current;
+        current = current->next;
+        free(temp);
+    }
+}
+
+int main() {
+    struct Node* head = NULL;
+    int data, numNodes;
+
+    printf("How many nodes do you want to create? ");
+    scanf("%d", &numNodes);
+
+    for (int i = 0; i < numNodes; i++) {
+        printf("Enter data for node %d: ", i + 1);
+        scanf("%d", &data);
+        insertAtEnd(&head, data);
+    }
+
+    printf("Linked List: ");
+    displayList(head);
+
+    freeList(head); // Free the memory
+
+    return 0;
+}
